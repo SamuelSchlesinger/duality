115677f587eea2137cdbc5a06a0d43b292ee9a98
Samuel Schlesinger <samuel@simspace.com>
2019-11-07 02:21:35 -0500

    couldn't resist doing a bit more

--- src/Control/Duality.hs
+++ src/Control/Duality.hs
@@ -40,7 +40,7 @@ instance Dual f g => Dual (Free f) (CoFree g) where
   zap z (Pure a) (CoFree b bs) = z a b
   zap z (Free a) (CoFree b bs) = zap (zap z) a bs
 
-type instance Co (CoFree f) = Free
+type instance Co (CoFree f) = Free (Co f)
 instance Dual g f => Dual (CoFree f) (Free g)
 
 data (f + g) x = L (f x) | R (g x)
@@ -81,7 +81,7 @@ type CoWriter a = ((->) a)
 
 -- Now we work in the category with functors as the objects and natural transformations
 -- between functors as the arrows.
-class Bifunctor t where
+class (forall f g. (Functor f, Functor g) => Functor (t f g)) => Bifunctor t where
   bimap :: forall f f' g g' a. 
     ( Functor f
     , Functor f'
@@ -127,6 +127,9 @@ instance Bidual (&) (+)
 
 newtype ComposeT h t (f :: * -> *) (g :: * -> *) a = ComposeT (h (t f g a))
 
+instance (Functor h, Bifunctor t, Functor f, Functor g) => Functor (ComposeT h t f g) where
+  fmap f (ComposeT a) = ComposeT $ fmap (fmap f) a
+
 instance (Functor h, Bifunctor t) => Bifunctor (ComposeT h t) where
   bimap n n' (ComposeT h) = ComposeT (fmap (bimap n n') h)
 
@@ -134,12 +137,18 @@ type instance Co (ComposeT h t) = ComposeT (Co h) (Co t)
 instance (Dual h h', Bidual t t') => Bidual (ComposeT h t) (ComposeT h' t') where
   bizap n n' (ComposeT a) (ComposeT b) = zap (bizap n n') a b
 
--- newtype ComposeS h t (f :: * -> *) (g :: * -> *) a = ComposeS (t f g (h a))
+newtype ComposeS h t (f :: * -> *) (g :: * -> *) a = ComposeS (t f g (h a))
 
--- instance (Functor h, Bifunctor t) => Bifunctor (ComposeS h t) where
---   bimap n n' (ComposeS h) = ComposeS (bimap n n' h)
+instance (Functor f, Functor g, Functor h, Bifunctor t) => Functor (ComposeS h t f g) where
+  fmap f (ComposeS h) = ComposeS (fmap (fmap f) h)
 
--- instance (Dual h h', Bidual t t') => Bidual (ComposeS h t) (ComposeS h' t') where
+instance (Functor h, Bifunctor t) => Bifunctor (ComposeS h t) where
+  bimap n n' (ComposeS h) = ComposeS (bimap n n' h)
+
+-- can't get the instance to work here...
+--instance (Functor f, Functor g, Dual h h', Bidual t t') => Dual (ComposeS h t f g) (ComposeS h' t' f g) where
+--  zap f (ComposeS a) (ComposeS b) = bizap (zap f) (zap f) a b
+
 -- Now this is weird...
 
 newtype FixT t a = FixT (t (FixT t) a)
@@ -152,7 +161,7 @@ type Machine m a b = FixT (ComposeT ((->) a) (&) m) b
 example :: Machine IO Int ()
 example = FixT . ComposeT $ \n -> P (print n) example
 
-type instance Co (FixT f) = Co (FixT (Co f)) 
+type instance Co (FixT f) = FixT (Co f) 
 instance (forall f f'. Dual f f' => Dual (t f) (t' f') 
         , forall f. Functor f => Functor (t' f)
         , forall f. Functor f => Functor (t f) ) => Dual (FixT t) (FixT t') where
@@ -163,24 +172,29 @@ newtype Codensity m a = Codensity (forall b. (a -> m b) -> m b)
 instance Functor (Codensity m) where
   fmap f (Codensity g) = Codensity \c -> g (c . f)
 
+liftC :: Monad m => m a -> Codensity m a
+liftC m = Codensity (m >>=)
+
 data Density m a where 
   Density :: (m b -> a) -> (m b) -> Density m a
+
+liftD :: Comonad w => w a -> Density w a
+liftD w = Density extract w
 
 instance Functor (Density m) where
   fmap f (Density g b) = Density (f . g) b
 
-type instance Co Density = Codensity
+type instance Co (Density w) = Codensity (Co w)
 instance (Comonad w, Monad m, Dual w m) => Dual (Density w) (Codensity m) where
   zap z (Density x g) (Codensity y) = zap z (extend x g) (y return)
 
-type instance Co Codensity = Density
+type instance Co (Codensity m) = Density (Co m)
 instance (Comonad w, Monad m, Dual w m) => Dual (Codensity m) (Density w) 
 
 -- Summary:
 --
 -- So now, I can write a free monad, a dual cofree comonadic context in which 
--- it will run, take the codensity/density transforms of each, and let the
--- computer do its thing.
+-- it will run, and let the computer do its thing.
 --
 -- Let's do it!
 
@@ -212,6 +226,8 @@ test = zap (flip const) discussion (coordinates (0, 0))
 -- you can record the input to all of your free monadic combinators, roll
 -- it up into a cofree monad, and replay history in a pure way to figure
 -- stuff out...
+
+-- TODO lets see what we can do with the codensity transformation!
 
 -- Things lieth beneath which are grokkable elsewhere
 --------------------------------------------------------------------------------
